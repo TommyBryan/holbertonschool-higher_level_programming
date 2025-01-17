c53229b87974b21771115e6446407be2bdbaecca
TommyBryan <tommy0713@gmail.com>
2025-01-17 15:43:29 -0400

    Enhance 0-positive_or_negative.py to classify random numbers as positive, negative, or zero

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+
 import random
+
 number = random.randint(-10, 10)
-print(number)
+
+if number > 0:
+    print(f"{number} is positive")
+elif number < 0:
+    print(f"{number} is negative")
+else:
+    print(f"{number} is zero")
