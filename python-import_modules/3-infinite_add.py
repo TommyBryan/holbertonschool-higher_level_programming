b30857baa10f3cffbc952b9eb0c389297ba15b27
TommyBryan <tommy0713@gmail.com>
2025-01-19 19:14:56 -0400

    Refactor infinite_add script to use sum function for cumulative total calculation

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -2,7 +2,5 @@
 if __name__ == "__main__":
     import sys
 
-    total = 0
-    for i in range(1, len(sys.argv)):
-        total += int(sys.argv[i])
-        print(total)
+    total = sum(int(arg) for arg in sys.argv[1:])
+    print(total)
