74a53b0743488a3f8816c829d3572069d32c668d
TommyBryan <tommy0713@gmail.com>
2025-01-24 15:30:57 -0400

    Fix return statement formatting in max_integer function

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -3,10 +3,10 @@
 def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
-    
+
     biggest_int = my_list[0]
     for i in my_list:
         if i > biggest_int:
             biggest_int = i
 
-    return biggest_int
+    return (biggest_int)
