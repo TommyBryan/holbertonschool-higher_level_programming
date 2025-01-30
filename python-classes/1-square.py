6ed98f3115c720bcf9a5d11c30ab48ad3ec9e917
TommyBryan <tommy0713@gmail.com>
2025-01-30 14:30:14 -0400

    Add Square class definition with initialization

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""creates a class Square"""
+
+
+class Square:
+    """defines a class Square."""
+
+    def __init__(self, size):
+        """Initializes the square with a private size attribute"""
+        self.__size = size
