891f393be277723af0c35a7507181ce2fe3b6773
TommyBryan <tommy0713@gmail.com>
2025-02-01 23:39:09 -0400

    Fix formatting in Square class and remove unnecessary whitespace

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -4,7 +4,7 @@
 
 class Square:
     """ defines a class Square """
-    
+
     def __init__(self, size=0):
         """ initializes the square with a private size attribute """
         self.__size = size
@@ -13,7 +13,7 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-        
+
     def area(self):
         """ returns the current square area """
         return self.__size ** 2
