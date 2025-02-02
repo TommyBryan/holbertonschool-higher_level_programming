07bb44439df15a6584e57fa102b933577f7b7769
TommyBryan <tommy0713@gmail.com>
2025-02-02 00:39:59 -0400

    Refactor Square class to use property for size attribute with validation

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -4,13 +4,22 @@
 
 class Square:
     def __init__(self, size=0):
-        """ Initialies the square with a private size attribute """
-        self.__size = size
-        if type(size) is not int:
+        """ Initializes the square with a private size attribute """
+        self.size = size
+
+    @property
+    def size(self):
+        """ Retrieves the size of the square """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ Sets the size of the square with validation """
+        if type(value) is not int:
             raise TypeError("size must be an integer")
-        if size < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        self.__size = value
 
     def area(self):
         """ Returns the current square area """
