4da246db6eb6c73bef5405104b9fc412c4933f7c
TommyBryan <tommy0713@gmail.com>
2025-02-02 15:56:33 -0400

    Refactor string representation method in Rectangle class for improved readability

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -52,4 +52,10 @@ class Rectangle:
         """ Returns the printable representation of the rectangle """
         if self.__width == 0 or self.__height == 0:
             return ("")
-        return ((("#" * self.__width + "\n") * self.__height)[:-1])
+        
+        rect = []
+        for i in range(self.__height):
+            [rect.append('#') for j in range(self.__width)]
+            if i != self.__height - 1:
+                rect.append("\n")
+        return ("".join(rect))
