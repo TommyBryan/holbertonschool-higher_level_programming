c2e175d36c451591794cb613ac00835db91fa78a
TommyBryan <tommy0713@gmail.com>
2025-02-02 00:50:37 -0400

    Refactor Square class to improve docstrings and encapsulate size attribute

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,26 +1,27 @@
 #!/usr/bin/python3
-""" Creates a class Square """
+"""creates class Square with
+private instance attribute size and public instance method"""
 
 
 class Square:
+    """defines class with instantiated and validated private instance attribute
+and public instance method."""
+
     def __init__(self, size=0):
-        """ Initializes the square with a private size attribute """
-        self.size = size
+        self.__size = size
 
     @property
     def size(self):
-        """ Retrieves the size of the square """
-        return self.__size
+        return (self.__size)
 
     @size.setter
     def size(self, value):
-        """ Sets the size of the square with validation """
         if type(value) is not int:
             raise TypeError("size must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
     def area(self):
-        """ Returns the current square area """
-        return self.__size ** 2
+        """calculates and returns current square area"""
+        return (self.__size * self.__size)
