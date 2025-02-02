78e682b454531d0a226fe46325ef5d6d2903f15e
TommyBryan <tommy0713@gmail.com>
2025-02-01 23:53:32 -0400

    Add Square class and main script for area demonstration

---
+++ python-classes/4-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Creates a class Square """
+
+
+class Square:
+    def __init__(self, size=0):
+        """ Initialies the square with a private size attribute """
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """ Returns the current square area """
+        return self.__size ** 2
