c44cd82ac995815f3d5505e9d60a6a1ba9518978
TommyBryan <tommy0713@gmail.com>
2025-02-08 17:13:42 -0400

    Fix area method to use private width attribute in Rectangle class

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -13,7 +13,7 @@ class Rectangle(BaseGeometry):
         self.__height = height
 
     def area(self):
-        return self.width * self.__height
+        return self.__width * self.__height
 
     def __str__(self):
         return f"[Rectangle] {self.__width}/{self.__height}"
