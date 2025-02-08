f88f9682e6e9dfd27bcce1c473b61b45480e059c
TommyBryan <tommy0713@gmail.com>
2025-02-07 23:05:18 -0400

    Add BaseGeometry class with area method raising exception

---
+++ python-inheritance/6-main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""
+Write a class BaseGeometry (based on 5-base_geometry.py).
+
+Public instance method: def area(self): that raises an Exception with the message area() is not implemented
+You are not allowed to import any module
+"""
+
+BaseGeometry = __import__('6-base_geometry').BaseGeometry
+
+bg = BaseGeometry()
+
+try:
+    print(bg.area())
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
+
+""" Output:
+[Exception] area() is not implemented
+"""
