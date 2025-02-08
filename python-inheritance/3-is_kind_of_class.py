94effd3b263c1db81c6c5cd47164af6ff8b214b6
TommyBryan <tommy0713@gmail.com>
2025-02-07 22:34:02 -0400

    Add docstring and complete implementation of is_kind_of_class function

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -3,4 +3,8 @@
 
 
 def is_kind_of_class(obj, a_class):
-    if 
+    """ Checks if object is an instanc or inherited instance of a class. """
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
