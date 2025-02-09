f4c66225360245aef4969d6c908b48b4875bacbf
TommyBryan <tommy0713@gmail.com>
2025-02-08 20:10:20 -0400

    Add area method and string representation to Square class

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -7,6 +7,13 @@ class Square(Rectangle):
     """ Inheritance """
 
     def __init__(self, size):
+        self.__size = size
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
+
+    def area(self):
+        return super().area()
+    
+    def __str__(self):
+        return (f"[Square] {self.__size}/{self.__size}")
