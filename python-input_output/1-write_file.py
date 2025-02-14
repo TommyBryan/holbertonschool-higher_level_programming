491dc0858b768fee6ca5796a6f27db345b8c9792
TommyBryan <tommy0713@gmail.com>
2025-02-14 14:34:38 -0400

    Remove unnecessary whitespace in write_file function

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -4,6 +4,6 @@
 
 def write_file(filename="", text=""):
     """ Appends a string at the end of a text file. """
-    
+
     with open(filename, "a", encoding="utf-8") as file:
         return file.write(text)
