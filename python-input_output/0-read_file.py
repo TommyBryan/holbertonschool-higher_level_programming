9fff61bcdedadb0411330d4576669a82c153b2ab
TommyBryan <tommy0713@gmail.com>
2025-02-14 14:15:08 -0400

    Fix encoding in read_file function to ensure proper text handling

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -4,5 +4,5 @@
 
 def read_file(filename=""):
     """ Fucntion that reads a text file and prints it to stdout. """
-    with open(filename, "r") as file:
+    with open(filename, "r", encoding="utf-8") as file:
         print(file.read())
