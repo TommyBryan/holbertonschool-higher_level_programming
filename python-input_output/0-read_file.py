10209c8b98e987da15b8dba5202f18708b3f20fa
TommyBryan <tommy0713@gmail.com>
2025-02-14 14:13:01 -0400

    Fix typo in read_file docstring and remove test call

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -6,6 +6,3 @@ def read_file(filename=""):
     """ Fucntion that reads a text file and prints it to stdout. """
     with open(filename, "r") as file:
         print(file.read())
-
-
-read_file("my_file_0.txt")
