3f0f87b620a9c5b5e8e9f3e56870016937e3c3aa
TommyBryan <tommy0713@gmail.com>
2025-02-14 14:11:35 -0400

    Add module docstring and fix typo in read_file function

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+""" Module for reading a file. """
+
 
 def read_file(filename=""):
+    """ Fucntion that reads a text file and prints it to stdout. """
     with open(filename, "r") as file:
         print(file.read())
 
