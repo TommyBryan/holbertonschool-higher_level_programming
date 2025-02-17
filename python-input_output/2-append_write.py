dabba5c1432a697475656c4723d4e8e4fea20a6b
TommyBryan <tommy0713@gmail.com>
2025-02-16 22:22:57 -0400

    Update docstring in append_write function for improved formatting and clarity

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -3,6 +3,8 @@
 
 
 def append_write(filename="", text=""):
-    """Appends a string to a text file and returns the number of characters added."""
+    """Appends a string to a text file and returns
+    the number of characters added.
+    """
     with open(filename, "a", encoding="utf-8") as file:
         return file.write(text)
