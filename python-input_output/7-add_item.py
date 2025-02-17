526044f874119dfc22f5136334c0fbc3c3a050ab
TommyBryan <tommy0713@gmail.com>
2025-02-16 22:21:14 -0400

    Refactor add_item script to enhance functionality and improve documentation

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,9 +1,26 @@
 #!/usr/bin/python3
-"""Module for creating an object from a JSON file."""
-import json
+"""Script that adds command-line arguments to a list
+and saves them to a file.
+"""
 
 
-def load_from_json_file(filename):
-    """Creates an object from a JSON file."""
-    with open(filename, "r", encoding="utf-8") as file:
-        return json.load(file)
+import sys
+import os
+
+save_to_json_file = __import__(
+    "5-save_to_json_file"
+).save_to_json_file
+load_from_json_file = __import__(
+    "6-load_from_json_file"
+).load_from_json_file
+
+filename = "add_item.json"
+
+if os.path.exists(filename):
+    my_list = load_from_json_file(filename)
+else:
+    my_list = []
+
+my_list.extend(sys.argv[1:])
+
+save_to_json_file(my_list, filename)
