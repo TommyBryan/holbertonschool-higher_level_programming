d1b1b4b293284609088ecf05561f8876b54d0610
TommyBryan <tommy0713@gmail.com>
2025-03-08 15:47:28 -0400

    Add SQL script to create second_table and insert multiple rows

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,15 @@
+-- Creates a table second_table in the database hbtn_0c_0 and add multiple rows
+-- second_table:
+-- id name score
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT,
+    name VARCHAR(256),
+    score INT
+);
+
+-- Insert multiple rows into the table second_table from the database hbtn_0c_0
+INSERT INTO second_table (id, name, score) VALUES
+    (1, 'John', 10),
+    (2, 'Alex', 3),
+    (3, 'Bob', 7),
+    (4, 'George', 8);
