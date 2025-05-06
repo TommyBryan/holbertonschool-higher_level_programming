542e5370341cd2cd7521c2379ceafddd366f990f
TommyBryan <tommybryan0713@gmail.com>
2025-05-06 11:13:44 -0400

    Added rest of file

--- python-hello_world/3-print_number.py
+++ python-hello_world/3-print_number.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 number = 98
-print(f"{number:d} Battery street")
-# The code prints the number 98 followed by the string "Battery street".
+print(f"{number} Battery street")
---
+++ python-hello_world/5-print_string.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+str = "Holberton School"
+print(str * 3)  # Adds a space after repetition
+print(str[:9])  # Prints str from the beginning (index 0) up to 9th index
