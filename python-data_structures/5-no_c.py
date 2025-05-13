0cbd420a137efa1bcc7bf37d39b4658d005ab8ec
TommyBryan <tommybryan0713@gmail.com>
2025-05-13 13:39:45 -0400

    Task 5

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -2,9 +2,4 @@
 
 def no_c(my_string):
 
-    # Iterates through the string and removes 'c' and 'C'
-    for char in my_string:
-        if char == 'c' or char == 'C':
-            # Replaces 'c' and 'C' with an empty string
-            my_string = my_string.replace(char, "")
-    return my_string
+    return ''.join([char for char in my_string if char != 'C' and char != 'c'])
