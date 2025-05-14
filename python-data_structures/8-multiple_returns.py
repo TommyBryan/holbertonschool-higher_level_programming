2d408037293e2bfb3c513d2d614870cc7b747fb8
TommyBryan <tommybryan0713@gmail.com>
2025-05-14 14:40:23 -0400

    Fixed file 8 pycodestyle

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    
+
     # Check if the sentence is empty
     for i in range(len(sentence)):
         # if the sentence is empty return None
