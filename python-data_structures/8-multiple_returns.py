e5ee5a2f4fb365dd2913a1ed7743edfb16debd47
TommyBryan <tommybryan0713@gmail.com>
2025-05-14 14:43:29 -0400

    Changed file 8

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-
-    # Check if the sentence is empty
-    for i in range(len(sentence)):
-        # if the sentence is empty return None
-        if sentence == "":
-            return None
-        # return the length of the sentence and the first character
-    return (len(sentence), sentence[0])
+    """
+    Returns a tuple with the length of the sentence and its first character.
+    If the sentence is empty, the first character is None.
+    """
+    if sentence == "":  # Check if the sentence is empty
+        return (0, None)  # Return length 0 and None as the first character
+    return (len(sentence), sentence[0])  # Return length and first character
