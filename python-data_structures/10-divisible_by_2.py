a067ee165a784f41f5fd30fab5e5dbc381217ee7
TommyBryan <tommybryan0713@gmail.com>
2025-05-14 16:09:47 -0400

    Fixed file 10 for pycodestyle validation

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 def divisible_by_2(my_list=[]):
-    
+
     new_list = []
-    
+
     for i in range(len(my_list)):  # Iterate through the list
         if my_list[i] % 2 == 0:  # Check if the element is divisible by 2
             new_list.append(True)  # Append True if divisible by 2
