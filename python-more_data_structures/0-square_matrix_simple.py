49a1bebc12e7c7b2ee3af977bdd360743878bc30
TommyBryan <tommybryan0713@gmail.com>
2025-05-16 14:38:33 -0400

    Task 0

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
 
-new_matrix = []
-    for row in new_matrix:
-    
+def square_matrix_simple(matrix=[]):
+    new_matrix = []  # Empty list to hold squared values
+    for row in matrix:
+        # Iterate through each row and square each element]
+        squared_row = [x ** 2 for x in row]
+        new_matrix.append(squared_row)
+    return new_matrix
