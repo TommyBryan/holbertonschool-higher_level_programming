1c16ef3fa402bfa22c5ebe26d9890790c0352543
TommyBryan <tommy0713@gmail.com>
2025-05-18 12:53:04 -0400

    Task 1

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
-    
+    new_list = []
+    for i in my_list:  # iterate through the list
+        if i == search:  # if element equals search, append replace
+            new_list.append(replace)
+        else:
+            new_list.append(i)
+    return new_list
