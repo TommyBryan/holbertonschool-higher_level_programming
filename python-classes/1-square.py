96c68eb94683bc8f9a4519a57be6abcc62e12833
TommyBryan <tommybryan0713@gmail.com>
2025-05-21 11:05:34 -0400

    Task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Creates a class Square. """
+
+
+class Square:
+    """ Defines a square by its size. """
+
+    def __init__(self, size):
+        """ Initializes the square with a private size attribute."""
+        self.__size = size
