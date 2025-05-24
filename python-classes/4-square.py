c7074f00fc85818d5cf9d58d752af642daef6eb1
TommyBryan <tommybryan0713@gmail.com>
2025-05-24 11:11:33 -0400

    Fixed file 4

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -6,21 +6,22 @@ class Square:
     """ Defines a square by its size. """
 
     def __init__(self, size=0):
-        """ Initializes the square with a private size attribute."""
-        self.__size = size
+        """ Initializes the square with a private size attribute. """
+        self.size = size  # Use the setter for validation
 
-        @property  # Property decorator
-        def size(self):
-            return (self.__size)
+    @property
+    def size(self):
+        """ Retrieves the size of the square. """
+        return self.__size
 
-        @size.setter  # Setter decorator
-        def size(self, value):
-            """ Checks if size is an integer. """
-            if type(size) is not int:
-                raise TypeError("size must be an integer")
-            if size < 0:
-                raise ValueError("size must be >= 0")
-            self.__size = value
+    @size.setter
+    def size(self, value):
+        """ Sets the size of the square with validation. """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
 
     def area(self):
         """ Returns the area of the square. """
