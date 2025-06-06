8ebaf710f6ff06f4b32d4efb3f39ede0f9e75f0c
TommyBryan <tommybryan0713@gmail.com>
2025-06-06 10:52:10 -0400

    Fixed documentation in file 0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -3,5 +3,6 @@
 
 
 def read_file(filename=""):
+    """ Opens a file in read mode in utf-8 encoding and prints its content."""
     with open(filename, "r", encoding="utf-8") as file:
         print(file.read(), end="")
