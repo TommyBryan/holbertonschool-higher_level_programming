1ef518df255f9aeb74c2ad0d3442714c9d45405e
TommyBryan <tommybryan0713@gmail.com>
2025-06-24 15:04:43 -0400

    Task 4 and 5

---
+++ SQL_introduction/5-full_table.sql
@@ -0,0 +1,6 @@
+-- Script that prints the ollowing description of the table first_table from the database hbtn_0c_0 in your MySQL server.
+
+--The database name will be passed as an argument of the mysql command
+--You are not allowed to use the DESCRIBE or EXPLAIN statements
+
+SHOW CREATE TABLE first_table; -- The CREATE statement is used to show the structure of the table including its columns and their data types.
