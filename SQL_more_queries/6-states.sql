c34d56cd84a9dab31b63770cbcff90b98146a6f8
TommyBryan <tommybryan0713@gmail.com>
2025-06-26 16:38:22 -0400

    TAsk 6 test run

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,5 +1,8 @@
 -- Create database hbtn_0d_usa and table states
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IN hbtn_0d_usa IF NOT EXISTS states (
-    id INT UNIQUE 
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY(id)
 )
