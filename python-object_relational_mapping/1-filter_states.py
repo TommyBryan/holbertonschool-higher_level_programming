56406e738f69577dad3eb866db5c79c3d4c6d6b6
TommyBryan <tommybryan0713@gmail.com>
2025-06-29 15:25:03 -0400

    Task 1.1

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-""" List all states starting with 'N' from the database hbtn_0e_0_usa."""
+"""
+List all states starting with 'N' from the database hbtn_0e_0_usa.
+"""
 
 import MySQLdb
 import sys
+
 
 if __name__ == "__main__":
     # Get MySQL credentials and database name from command-line arguments
