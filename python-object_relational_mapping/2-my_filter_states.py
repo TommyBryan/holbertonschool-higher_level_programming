4e28b47bdf9e69cbc73f02e2c1f4e4a7b63aa5dc
TommyBryan <tommybryan0713@gmail.com>
2025-06-29 15:52:10 -0400

    test

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,36 +1,37 @@
 #!/usr/bin/python3
 """
-Script that takes in an argument and displays all values in the states table
-where name matches the argument.
+Script that displays all values in the states table
+where name matches the argument
 """
 
 import MySQLdb
 import sys
 
+
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
     state_name = sys.argv[4]
 
-    # Connect to DB
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=username,
-        passwd=password,
-        db=db_name
+        user=mysql_username,
+        passwd=mysql_password,
+        db=database_name
     )
 
     cursor = db.cursor()
 
-    # Vulnerable query using string formatting
-    query = "SELECT * FROM states WHERE name = BINARY '{}' ORDER BY id ASC".format(state_name)
+    query = "SELECT * FROM states \
+        WHERE name = BINARY '{}' ORDER BY id ASC".format(state_name)
     cursor.execute(query)
 
-    # Fetch and display results
-    for row in cursor.fetchall():
-        print(row)
+    states = cursor.fetchall()
+
+    for state in states:
+        print(state)
 
     cursor.close()
     db.close()
