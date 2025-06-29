dc21020f8bcae393bb64bc34134df3b308e85369
TommyBryan <tommybryan0713@gmail.com>
2025-06-29 14:34:37 -0400

    idk

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -11,7 +11,7 @@ import sys
 if __name__ == "__main__":
     # Get MySQL credentials and database from command-line arguments
     mysql_username = sys.argv[1]
-    mysql_password = sys.argv[1]
+    mysql_password = sys.argv[2]
     database_name = sys.argv[3]
 
     # Connect to MySQL database on localhost:3306
