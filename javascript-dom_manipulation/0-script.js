be3e0cf1428d91cf687015b422896caaef41570e
TommyBryan <tommybryan0713@gmail.com>
2025-07-18 14:32:27 -0400

    Task 0 - 1

--- javascript-dom_manipulation/0-script.js
+++ javascript-dom_manipulation/0-script.js
@@ -1,1 +1,2 @@
+// Update  text color of header element to red #FF0000
 document.querySelector('header').style.color = '#FF0000';
---
+++ javascript-dom_manipulation/1-script.js
@@ -0,0 +1,15 @@
+/*
+Updates the text color of header to red #FF0000
+When user clicks on the tag with the id red_header
+*/
+
+    // Waits for page to load
+document.addEventListener("DOMContentLoaded", function () {
+    // Get the button with id red_header
+    const button = document.getElementById("red_header");
+
+// Waits for user to click button to change the color of header text
+button.addEventListener("click", function () {
+    document.querySelector('header').style.color = '#FF0000';
+});
+});
