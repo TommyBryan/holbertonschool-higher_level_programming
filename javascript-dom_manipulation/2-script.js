29cbb4a27290994145cb8f7e16f1ffaa5ef973d3
TommyBryan <tommy0713@gmail.com>
2025-07-19 11:11:01 -0400

    Task 2

--- javascript-dom_manipulation/2-script.js
+++ javascript-dom_manipulation/2-script.js
@@ -1,7 +1,14 @@
+// wait for page to load
 document.addEventListener('DOMContentLoaded', function () {
+  // select the <div> with id='red_header'
   const redHeader = document.getElementById('red_header');
 
-  button.addEventListener('click', function () {
+  // Select header element
+  const header = document.querySelector('header');
 
+  // Click event listener to redHeader div
+  redHeader.addEventListener('click', function () {
+    //  Add class 'red' to <header>
+    header.classList.add('red');
   });
 });
