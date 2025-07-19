29de15b481e58be3c43122b9556de8bb96d01aa2
TommyBryan <tommybryan0713@gmail.com>
2025-07-19 10:20:28 -0400

    Made changes in file 1 - 2

--- javascript-dom_manipulation/1-script.js
+++ javascript-dom_manipulation/1-script.js
@@ -6,10 +6,10 @@ When user clicks on the tag with the id red_header
 // Waits for page to load
 document.addEventListener('DOMContentLoaded', function () {
   // Get the button with id red_header
-  const button = document.getElementById('red_header');
+  const redHeader = document.getElementById('red_header');
 
   // Waits for user to click button to change the color of header text
-  button.addEventListener('click', function () {
+  redHeader.addEventListener('click', function () {
     document.querySelector('header').style.color = '#FF0000';
   });
 });
