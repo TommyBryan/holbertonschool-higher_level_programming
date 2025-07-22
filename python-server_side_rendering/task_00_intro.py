08fc17b2cd3554f94025d0279a733dd05b281907
TommyBryan <tommybryan0713@gmail.com>
2025-07-22 15:51:34 -0400

    helpp

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -20,10 +20,14 @@ def generate_invitations(template, attendees):
         print("Error: Attendees must be a list of dictionaries")
         return
 
+    if not template.strip():
+        print("Template is empty, no output files generated")
+        return
+    
     if not attendees:
         print("No data provided, no output files generated.")
         return
-    
+
     for i, attendee in enumerate(attendees, start=1):
         output_file = f"output_{i}.txt"
         form_fillout = {
