c2bf50f9ed333c8af35e4dca71018b04be8921eb
TommyBryan <tommy0713@gmail.com>
2025-07-21 20:26:34 -0400

    Task 0

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -1,2 +1,16 @@
+import os
+
+"""
+Function called: generate_invitations that takes tow parameters
+    template: str
+    attendees: list of dictionaries
+"""
+
 def generate_invitations(template, attendees):
-    
+    #  Check input types
+    if not isinstance(template, str):
+        print("Error: Template must be a string.")
+        return
+    if not isinstance(attendees, list):
+        print("Error: Attendees must be a list of dictionaries.")
+
