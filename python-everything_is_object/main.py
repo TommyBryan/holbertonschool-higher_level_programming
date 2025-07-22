b30d8e5d1e69ace0d60b81d186fc7938c016d1cc
TommyBryan <tommybryan0713@gmail.com>
2025-07-22 14:53:44 -0400

    Task 19

--- python-everything_is_object/main.py
+++ python-everything_is_object/main.py
@@ -1,7 +1,16 @@
-s1 = "Best"
-s2 = s1
-print(s1 is s2)
+s1 = "Best School"
+s2 = "Best School"
+print(s1 is s2)         # Likely True
+
+s3 = "Best " + "School" # Still likely interned
+print(s1 is s3)         # Might be True
+
+s4 = "".join(["Best", " School"])
+print(s1 is s4)         # Usually False (not interned)
+
 
 l1 = [1,2,3]
 l2 = [1,2,3]
 print(l1 is l2)
+
+#
